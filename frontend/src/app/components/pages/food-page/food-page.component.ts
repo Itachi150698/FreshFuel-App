1ced58d9ab8e9adb00a8e85a93a256dd117ee31b
Itachi150698 <150852439+Itachi150698@users.noreply.github.com>
2024-04-05 11:59:40 +0530

    Update food-page.component.ts

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
+import { Tag } from 'src/app/shared/models/Tag'; // Import Tag model if it's defined somewhere
 
 @Component({
   selector: 'app-food-page',
@@ -11,21 +12,45 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class FoodPageComponent implements OnInit {
   food!: Food;
-  constructor(activatedRoute:ActivatedRoute, foodService:FoodService,
-    private cartService:CartService, private router: Router) {
+  tags?: Tag[];
+  searchTerm = '';
+   
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
     activatedRoute.params.subscribe((params) => {
-      if(params.id)
-      foodService.getFoodById(params.id).subscribe(serverFood => {
-        this.food = serverFood;
-      });
-    })
-   }
+      if (params.id) {
+        foodService.getFoodById(params.id).subscribe(serverFood => {
+          this.food = serverFood;
+        });
+      }
+      if (params.searchTerm) {
+        this.searchTerm = params.searchTerm;
+      }
+    });
+
+    foodService.getAllTags().subscribe(serverTags => {
+      this.tags = serverTags;
+    });
+  }
 
   ngOnInit(): void {
   }
 
-  addToCart(){
+  search(term: string): void {
+    if (term) {
+      this.router.navigateByUrl('/search/' + term);
+    }
+  }
+
+  addToCart() {
     this.cartService.addToCart(this.food);
     this.router.navigateByUrl('/cart-page');
   }
+
+
 }
